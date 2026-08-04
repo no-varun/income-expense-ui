16f3858227ba0df239239caf7e95a6c73a28b2ca
varun chaudhary <81312002+no-varun@users.noreply.github.com>
2026-08-04 16:48:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-        },
-      },
-    },
+    chunkSizeWarningLimit: 1000,
   },
 });
