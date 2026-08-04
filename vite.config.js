8cb2d5410201c545848cdb1177c0b2d9579e9718
varun chaudhary <81312002+no-varun@users.noreply.github.com>
2026-08-04 17:40:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules/react")) {
+            return "vendor";
+          }
+        },
+      },
+    },
   },
 });
